83d484e6289994bbdd0b28d9faeab6369380182a
Brian Smith <briaguya@gmail.com>
2015-08-30 23:16:42 -0700

    fixed some js refs

--- application/views/footer.php
+++ application/views/footer.php
@@ -15,13 +15,13 @@
 <!-- Le javascript
 ================================================== --> 
 <!-- Placed at the end of the document so the pages load faster --> 
-<script src="/js/jquery-1.7.2.min.js"></script> 
-<script src="/js/excanvas.min.js"></script> 
-<script src="/js/chart.min.js" type="text/javascript"></script> 
-<script src="/js/bootstrap.js"></script>
-<script language="javascript" type="text/javascript" src="/js/full-calendar/fullcalendar.min.js"></script>
+<script src="/sleepover/js/jquery-1.7.2.min.js"></script>
+<script src="/sleepover/js/excanvas.min.js"></script>
+<script src="/sleepover/js/chart.min.js" type="text/javascript"></script>
+<script src="/sleepover/js/bootstrap.js"></script>
+<script language="javascript" type="text/javascript" src="/sleepover/js/full-calendar/fullcalendar.min.js"></script>
  
-<script src="/js/base.js"></script> 
+<script src="/sleepover/js/base.js"></script>
 
 <?
 if($page == "reservation" ) {

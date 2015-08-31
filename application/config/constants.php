054f598de591c7c1300d88dfc3b6097858807bd7
Brian Smith <briaguya@gmail.com>
2015-08-30 23:21:04 -0700

    removed HOTEL_NAME const

--- application/config/constants.php
+++ application/config/constants.php
@@ -1,10 +1,5 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-
-
-define('HOTEL_NAME', "DedeBan Hotel");
-
-
 /*
 |--------------------------------------------------------------------------
 | File and Directory Modes

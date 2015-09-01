a484d052ed02a8ed90a424c0d7e293bcc586e5fa
Brian Smith <briaguya@gmail.com>
2015-08-31 19:40:52 -0700

    ?

--- application/views/employee/list.php
+++ application/views/employee/list.php
@@ -1,1 +1,41 @@
-<?php if(isset($employees)) {echo "WE HAVE A VARIABLE";} else {echo "NOPE";}
+<div class="main">
+  <div class="main-inner">
+    <div class="container">
+      <div class="row">
+        <div class="span12">
+			<a href="/sleepover/employee/add" class="btn btn-small btn-primary"><i class="btn-icon-only icon-ok"></i>Add Employee</a>
+			<br><br>
+			<table class="table table-striped table-bordered">
+				<thead>
+				  <tr>
+				    <th> Fullname </th>
+				    <th> Username </th>
+				    <th> Department </th>
+				    <th> Job </th>
+				    <th> Email </th>
+				    <th class="td-actions"> Actions </th>
+				  </tr>
+				</thead>
+				<tbody>
+				<?php
+					if(isset($employees))
+					{
+						foreach ($employees as $emp)
+						{?>
+						<tr>
+							<td> <?/*=$emp->employee_firstname ." ".$emp->employee_lastname*/?> </td>
+				    		<td> <?/*=$emp->employee_username */?> </td>
+				    		<td> <?/*=$emp->department_name */?> </td>
+				    		<td> <?/*=$emp->employee_type */?> </td>
+				    		<td> <?/*=$emp->employee_email */?> </td>
+				    		<td class="td-actions"><a href="/sleepover/employee/edit/<?/*=$emp->employee_id*/?>" class="btn btn-small btn-primary"><i class="btn-icon-only icon-edit"> </i></a><a href="/employee/delete/<?/*=$emp->employee_id*/?>" onclick="return confirm('Are you sure ?')" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
+						</tr><? }
+                    }
+                ?>
+				</tbody>
+			</table>
+		</div>
+	  </div>
+	</div>
+  </div>
+</div>

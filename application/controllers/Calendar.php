0cde66e6b860e46cca249b0ca3a21916a4b643fb
Brian Smith <briaguya@gmail.com>
2015-09-01 02:10:16 -0700

    check login on calendar

--- application/controllers/Calendar.php
+++ application/controllers/Calendar.php
@@ -1,9 +1,18 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Calendar extends CI_Controller {
-public function index()
-{
-    $this->load->view('header', array('title' => 'sleepover - Calendar', 'page' => 'calendar'));
-    $this->load->view('calendar');
-    $this->load->view('footer');
-}}
+    public function check_login()
+    {
+        if(!UID)
+            redirect("login");
+    }
+
+    public function index()
+    {
+        $this->check_login();
+
+        $this->load->view('header', array('title' => 'sleepover - Calendar', 'page' => 'calendar'));
+        $this->load->view('calendar');
+        $this->load->view('footer');
+    }
+}

86b8aa6f98ff31f07a039866fb8828823d37a148
Brian Smith <briaguya@gmail.com>
2015-08-31 19:01:23 -0700

    employee table fix?

--- application/views/employee/list.php
+++ application/views/employee/list.php
@@ -18,18 +18,20 @@
 				</thead>
 				<tbody>
 				<?
-					foreach ($employees as $emp) {
-						// $emp->username
-				?>
-				  <tr>
-				    <td> <?=$emp->employee_firstname ." ".$emp->employee_lastname?> </td>
-				    <td> <?=$emp->employee_username ?> </td>
-				    <td> <?=$emp->department_name ?> </td>
-				    <td> <?=$emp->employee_type ?> </td>
-				    <td> <?=$emp->employee_email ?> </td>
-				    <td class="td-actions"><a href="/employee/edit/<?=$emp->employee_id?>" class="btn btn-small btn-primary"><i class="btn-icon-only icon-edit"> </i></a><a href="/employee/delete/<?=$emp->employee_id?>" onclick="return confirm('Are you sure ?')" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
-				  </tr>
-				<? } ?>
+					if(isset($employees))
+					{
+						foreach ($employees as $emp)
+						{?>
+						<tr>
+							<td> <?=$emp->employee_firstname ." ".$emp->employee_lastname?> </td>
+				    		<td> <?=$emp->employee_username ?> </td>
+				    		<td> <?=$emp->department_name ?> </td>
+				    		<td> <?=$emp->employee_type ?> </td>
+				    		<td> <?=$emp->employee_email ?> </td>
+				    		<td class="td-actions"><a href="/employee/edit/<?=$emp->employee_id?>" class="btn btn-small btn-primary"><i class="btn-icon-only icon-edit"> </i></a><a href="/employee/delete/<?=$emp->employee_id?>" onclick="return confirm('Are you sure ?')" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
+						</tr><? }
+                    }
+                ?>
 				</tbody>
 			</table>
 		</div>

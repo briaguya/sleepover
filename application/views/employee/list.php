58675ae364a4aed4077c7f2fb4aee40a810479f4
Brian Smith <briaguya@gmail.com>
2015-08-31 20:56:14 -0700

    huh?

--- application/views/employee/list.php
+++ application/views/employee/list.php
@@ -5,7 +5,7 @@
 				<div class="span12">
 					<a href="/sleepover/employee/add" class="btn btn-small btn-primary"><i class="btn-icon-only icon-ok"></i>Add Employee</a>
 					<br><br>
-					<? echo "This is where we'll put the table"; ?>
+					<?php echo "This is where we'll put the table"; ?>
 				</div>
 			</div>
 		</div>

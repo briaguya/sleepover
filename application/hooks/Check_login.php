d9ef4006a9e3bef84e9e6a9f4e6bcff0f9ed1b1a
Brian Smith <briaguya@gmail.com>
2015-09-02 00:01:15 -0700

    fixing header, putting hack back

--- application/hooks/Check_login.php
+++ application/hooks/Check_login.php
@@ -10,8 +10,7 @@ class Check_login{
 		
 		define('UID', $CI->session->userdata('uid'));
 		define('USERNAME', $CI->session->userdata('username'));
-		define('FULLNAME', $CI->session->userdata('fullname'));
-		define('DEPARTMENT_NAME', $CI->session->userdata('department_name'));
+		define('FULL_NAME', $CI->session->userdata('full_name'));
 
 		define("SHOW_GUIDE", !$CI->session->userdata('show_guide'));
 	}
--- application/views/header.php
+++ application/views/header.php
@@ -31,7 +31,7 @@
           <div class="nav-collapse">
             <ul class="nav pull-right">
                 <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><i
-                                  class="icon-user"></i> <?=full_name?> (<?=username?>) <b class="caret"></b></a>
+                                  class="icon-user"></i> <?=FULL_NAME?> (<?=USERNAME?>) <b class="caret"></b></a>
                   <ul class="dropdown-menu">
                     <li><a href="/sleepover/login/logout">Logout</a></li>
                   </ul>

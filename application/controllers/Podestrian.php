5523bde29fb60e7e691f6491f3f8f9e5c7d936d9
Brian Smith <briaguya@gmail.com>
2015-09-01 18:08:09 -0700

    merge add/edit pedestrian into one

--- application/controllers/Podestrian.php
+++ application/controllers/Podestrian.php
@@ -22,35 +22,6 @@ class Podestrian extends CI_Controller {
 	{
 		if(!UID)
 			redirect("login");
-	} 
-
-	public function add()
-	{
-		if($this->input->post("first_name") && $this->input->post("last_name") && $this->input->post("email"))
-		{
-			$first_name = $this->input->post("first_name");
-			$last_name = $this->input->post("last_name");
-			$email = $this->input->post("email");
-			$podestrian_type_id = $this->input->post("podestrian_type_id");
-			$address_id = $this->input->post("address_id");
-			$sex = $this->input->post("sex");
-			$facebook = $this->input->post("facebook");
-            $twitter = $this->input->post("twitter");
-            $instagram = $this->input->post("instagram");
-            $birthday = $this->input->post("birthday");
-            $how_found = $this->input->post("how_found");
-			
-			$this->podestrian_m->addPodestrian($first_name, $last_name, $email, $podestrian_type_id, $address_id, $sex, $facebook, $twitter, $instagram, $birthday, $how_found);
-			redirect("/podestrian");
-		}
-
-		$data = array('title' => 'sleepover - Add Podestrian', 'page' => 'podestrian');
-		$this->load->view('header', $data);
-		$podestrian_types = $this->podestrian_m->getPodestrianTypes();
-        $addresses = $this->podestrian_m->getAddresses();
-		$viewdata = array('podestrian_types' => $podestrian_types, 'addresses' => $addresses);
-		$this->load->view('podestrian/add',$viewdata);
-		$this->load->view('footer');
 	}
 
 	function delete($podestrian_id)
@@ -59,35 +30,43 @@ class Podestrian extends CI_Controller {
 		redirect("/podestrian");
 	}
 
-	public function edit($employee_id)
+    private function modify($podestrian_id)
+    {
+        if($this->input->post("first_name") && $this->input->post("last_name") && $this->input->post("email"))
+        {
+            $first_name = $this->input->post("first_name");
+            $last_name = $this->input->post("last_name");
+            $email = $this->input->post("email");
+            $podestrian_type_id = $this->input->post("podestrian_type_id");
+            $address_id = $this->input->post("address_id");
+            $sex = $this->input->post("sex");
+            $facebook = $this->input->post("facebook");
+            $twitter = $this->input->post("twitter");
+            $instagram = $this->input->post("instagram");
+            $birthday = $this->input->post("birthday");
+            $how_found = $this->input->post("how_found");
+
+            $this->podestrian_m->modify($podestrian_id, $first_name, $last_name, $email, $podestrian_type_id, $address_id, $sex, $facebook, $twitter, $instagram, $birthday, $how_found);
+            redirect("/podestrian");
+        }
+
+        $data = array('title' => 'sleepover - Add Podestrian', 'page' => 'podestrian');
+        $this->load->view('header', $data);
+        $podestrian_types = $this->podestrian_m->getPodestrianTypes();
+        $addresses = $this->podestrian_m->getAddresses();
+        $viewdata = array('podestrian_types' => $podestrian_types, 'addresses' => $addresses);
+        $this->load->view('podestrian/add',$viewdata);
+        $this->load->view('footer');
+    }
+
+    public function add()
+    {
+        $this->modify();
+    }
+
+	public function edit($podestrian_id)
 	{
-		if($this->input->post("username") && $this->input->post("password") && $this->input->post("email"))
-		{
-			$username = $this->input->post("username");
-			$password = $this->input->post("password");
-			$firstname = $this->input->post("firstname");
-			$lastname = $this->input->post("lastname");
-			$telephone = $this->input->post("telephone");
-			$email = $this->input->post("email");
-			$department_id = $this->input->post("department_id");
-			$type = $this->input->post("type");
-			$salary = $this->input->post("salary");
-			$hiring_date = $this->input->post("hiring_date");
-			
-			$this->team_member_m->editEmployee($employee_id, $username, $password, $firstname, $lastname, $telephone, $email, $department_id, $type, $salary, $hiring_date);
-			redirect("/team_member");
-		}
-		
-		$data = array('title' => 'Edit Employee - DB Hotel Management System', 'page' => 'team_member');
-		$this->load->view('header', $data);
-
-		$departments = $this->team_member_m->getDepartments();
-		$employee = $this->team_member_m->getEmployee($employee_id);
-		
-		$viewdata = array('departments' => $departments, 'team_member'  => $employee[0]);
-		$this->load->view('team_member/edit',$viewdata);
-
-		$this->load->view('footer');
+        $this->modify($podestrian_id);
 	}
 
 	public function index()
--- application/models/Podestrian_m.php
+++ application/models/Podestrian_m.php
@@ -22,26 +22,26 @@ class Podestrian_m extends CI_Model {
         return false;
     }
 
-    function addPodestrian($first_name, $last_name, $email, $podestrian_type_id, $address_id, $sex, $facebook, $twitter, $instagram, $birthday, $how_found)
+    function modifyPodestrian($podestrian_id, $first_name, $last_name, $email, $podestrian_type_id, $address_id, $sex, $facebook, $twitter, $instagram, $birthday, $how_found)
     {
-        //todo move this to a stored procedure
         $data = array('first_name' => $first_name, 'last_name' => $last_name, 'email' => $email, 'podestrian_type_id' => $podestrian_type_id, 'address_id' => $address_id, 'sex' => $sex, 'facebook' => $facebook, 'twitter' => $twitter, 'instagram' => $instagram, 'birthday' => $birthday, 'how_found' => $how_found);
-        $this->db->insert('podestrian', $data);
-        return $this->db->affected_rows();
+
+        //todo move this to a stored procedure
+        if($podestrian_id == null)
+        {
+            // we don't have a podestrian id, we're adding
+            $this->db->insert('podestrian', $data);
+            return $this->db->affected_rows();
+        }
+
+        $this->db->where('podestrian_id', $podestrian_id);
+        $this->db->update('podestrian', $data);
     } 
 
     function deletePodestrian($podestrian_id)
     {
         $this->db->delete('podestrian', array('podestrian_id' => $podestrian_id));
         return $this->db->affected_rows();
-    }
-
-    function editEmployee($employee_id, $username, $password, $firstname, $lastname, $telephone, $email, $department_id, $employee_type, $employee_salary, $employee_hiring_date)
-    {
-        $data = array('employee_username' => $username, 'employee_password' => $password, 'employee_firstname' => $firstname, 'employee_lastname' => $lastname, 'employee_telephone' => $telephone, 'employee_email' => $email, 'department_id' => $department_id, 'employee_type' => $employee_type, 'employee_salary' => $employee_salary, 'employee_hiring_date' => $employee_hiring_date);
-
-        $this->db->where('employee_id', $employee_id);
-        $this->db->update('team_member', $data);
     }
 
     function getEmployee($employee_id)

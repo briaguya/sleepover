6ef8910d10994c00fd1a3e87ad285f25cd625adf
Brian Smith <briaguya@gmail.com>
2015-09-01 19:33:23 -0700

    make name not editable

--- application/views/podestrian/modify.php
+++ application/views/podestrian/modify.php
@@ -15,20 +15,28 @@
 
 				<div>todo:pic</div>
 
-				<div class="field">
-					<label for="first_name">First Name:</label>
-					<input type="text" id="first_name" name="first_name" required value=""/>
-				</div>
+                <? if($podestrian_id == null) {?>
+				<?= "<div class=\"field\">" ?>
+					<?= "<label for=\"first_name\">First Name:</label>" ?>
+					<?= "<input type=\"text\" id=\"first_name\" name=\"first_name\" required value=\"\"/>" ?>
+				<?= "</div>" ?>
 
-				<div class="field">
-					<label for="last_name">Last Name:</label>
-					<input type="text" id="last_name" name="last_name" required value=""/>
-				</div>
+				<?= "<div class=\"field\>" ?>
+					<?= "<label for=\"last_name\">Last Name:</label>" ?>
+					<?= "<input type=\"text\" id=\"last_name\" name=\"last_name\" required value=\"\"/>" ?>
+				<?= "</div>" ?>
 
-				<div class="field">
-					<label for="email">Email Address:</label>
-					<input type="text" id="email" name="email" required value=""/>
-				</div>
+				<?= "<div class=\"field\">" ?>
+					<?= "<label for=\"email\">Email Address:</label>" ?>
+					<?= "<input type=\"text\" id=\"email\" name=\"email\" required value=\"\"/>" ?>
+				<?= "</div>" ?>
+                <?} else {?>
+                <?= "<h2>" ?>
+                    <?=$podestrian->first_name ." ".$podestrian->last_name?>
+                <?= "</h2>" ?>
+                <?= "<h2>" ?>
+                    <?=$podestrian->email ?>
+                <?= "</h2>"; }?>
 
 				<div class="field">
 					<label for="podestrian_type_id">Podestrian Type:</label>

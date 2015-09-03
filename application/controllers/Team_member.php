3bd31294b9330583d61b14af3d4bfcb514b6ad68
Brian Smith <briaguya@gmail.com>
2015-09-02 17:50:53 -0700

    breaking up save and modify for team_member

--- application/controllers/Team_member.php
+++ application/controllers/Team_member.php
@@ -29,46 +29,58 @@ class Team_member extends CI_Controller {
         $this->podestrian_m->deletePodestrian($podestrian_id);
         redirect("/podestrian");
     }
-
+    
     public function modify($team_id = null)
     {
         if($team_id == null)
         {
-            // We're adding
-            if($this->input->post("username") && $this->input->post("password") && $this->input->post("podestrian_id"))
-            {
-                //We're submitting a new team member
-                $team_member = array(
-                    'username' => $this->input->post("username"),
-                    'password' => $this->input->post("password"),
-                    'podestrian_id' => $this->input->post("podestrian_id"),
-                    'role' => $this->input->post("role_id"));
-
-                $this->team_member_m->modify($team_member);
-                redirect("/team_member");
-            }
-
-            // We're filling out the team member form, we need a null team member
+            //We're adding, we need a null team_member
             $team_member = null;
             $data = array('title' => 'sleepover - Add Team Member', 'page' => 'team_member');
         }
         else
         {
-            // We're editing, we want to get the podestrian
+            // We're editing, we want to get the team_member
             $team_member = $this->team_member_m->getTeamMember($team_id);
             $data = array('title' => 'sleepover - Edit Team Member', 'page' => 'team_member');
         }
 
-        $this->load->view('header', $data);
         $podestrians = $this->podestrian_m->get_podestrians();
-        //$roles = $this->team_member_m->getRoles();
+        $roles = $this->team_member_m->getRoles();
         $viewdata = array(
             'podestrians' => $podestrians,
-            'roles' => null,
+            'roles' => $roles,
             'team_id' => $team_id,
             'team_member' => $team_member[0]);
         $this->load->view('team_member/modify',$viewdata);
         $this->load->view('footer');
+    }
+
+    public function save($team_id = null)
+    {
+        if($team_id == null)
+        {
+            //we need username, password, and podestrian_id, if we don't have them return
+            if(!($this->input->post("username") && $this->input->post("password") && $this->input->post("podestrian_id")))
+                return; //todo error?
+
+            //We're adding, make a new team member
+            $team_member = array(
+                'username' => $this->input->post("username"),
+                'password' => $this->input->post("password"),
+                'podestrian_id' => $this->input->post("podestrian_id"),
+                'role' => $this->input->post("role_id"));
+        }
+        else
+        {
+            // We're editing
+            $team_member = array(
+                'team_id' => $team_id,
+                'role' => $this->input->post("role_id"));
+        }
+
+        $this->team_member_m->save($team_member);
+        redirect("/team_members");
     }
 
     public function index()

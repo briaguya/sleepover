c443f83d4d524730dee14048de28f0bf76b406b4
Brian Smith <briaguya@gmail.com>
2015-09-02 17:25:58 -0700

    null checks for add mode

--- application/views/podestrian/modify.php
+++ application/views/podestrian/modify.php
@@ -41,7 +41,8 @@
 					<label for="podestrian_type_id">Podestrian Type:</label>
 					<select id="podestrian_type_id" name="podestrian_type_id">
 						<? foreach ($podestrian_types as $type) { ?>
-							<option value="<?=$type->podestrian_type_id?>" <? if($type->podestrian_type_id==$podestrian->podestrian_type_id) { echo "selected"; } ?>><?=$type->podestrian_type?></option>
+							<option value="<?=$type->podestrian_type_id?>"
+                                <? if($podestrian_id != null) { if($type->podestrian_type_id==$podestrian->podestrian_type_id) { echo "selected";}}?>><?=$type->podestrian_type?></option>
 						<? } ?>
 					</select>
 				</div>
@@ -50,7 +51,7 @@
 					<label for="address_id">Address:</label>
 					<select id="address_id" name="address_id">
 						<? foreach ($addresses as $address) { ?>
-							<option value="<?=$address->address_id?>" <? if($address->address_id==$podestrian->address_id) { echo "selected"; } ?>><?=$address->city?>, <?=$address->country?></option>
+							<option value="<?=$address->address_id?>" <? if($podestrian_id != null) { if($address->address_id==$podestrian->address_id) { echo "selected"; }} ?>><?=$address->city?>, <?=$address->country?></option>
 						<? } ?>
 					</select>
 				</div>
@@ -58,35 +59,35 @@
                 <div class="field">
                     <label for="sex">Sex:</label>
                     <select id="sex" name="sex">
-                        <option value ="Female" <? if($podestrian->sex=="Female") { echo "selected"; } ?>>Female</option>
-                        <option value ="Male" <? if($podestrian->sex=="Male") { echo "selected"; } ?>>Male</option>
-                        <option value ="Not Applicable" <? if($podestrian->sex=="Not Applicable") { echo "selected"; } ?>>Not Applicable</option>
+                        <option value ="Female" <? if($podestrian_id != null) { if($podestrian->sex=="Female") { echo "selected"; }} ?>>Female</option>
+                        <option value ="Male" <? if($podestrian_id != null) { if($podestrian->sex=="Male") { echo "selected"; }} ?>>Male</option>
+                        <option value ="Not Applicable" <? if($podestrian_id != null) { if($podestrian->sex=="Not Applicable") { echo "selected"; }} ?>>Not Applicable</option>
                     </select>
                 </div>
 
                 <div class="field">
                     <label for="facebook">Facebook:</label>
-                    <input type="text" id="facebook" name="facebook" value="<?=$podestrian->facebook?>"/>
+                    <input type="text" id="facebook" name="facebook" value="<? if($podestrian_id != null) { echo $podestrian->facebook;}?>"/>
                 </div>
 
                 <div class="field">
                     <label for="twitter">Twitter:</label>
-                    <input type="text" id="twitter" name="twitter" value="<?=$podestrian->twitter?>"/>
+                    <input type="text" id="twitter" name="twitter" value="<? if($podestrian_id != null) { echo $podestrian->twitter;}?>"/>
                 </div>
 
                 <div class="field">
                     <label for="instagram">Instagram:</label>
-                    <input type="text" id="instagram" name="instagram" value="<?=$podestrian->instagram?>"/>
+                    <input type="text" id="instagram" name="instagram" value="<? if($podestrian_id != null) { echo $podestrian->instagram;}?>"/>
                 </div>
 
                 <div class="field">
                     <label for="birthday">Birthday:</label>
-                    <input type="date" id="birthday" name="birthday" value="<?=$podestrian->birthday?>"/>
+                    <input type="date" id="birthday" name="birthday" value="<? if($podestrian_id != null) { echo $podestrian->birthday;}?>"/>
                 </div>
 
                 <div class="field">
                     <label for="how_found">How they found us:</label>
-                    <input type="text" id="how_found" name="how_found" value="<?=$podestrian->how_found?>"/>
+                    <input type="text" id="how_found" name="how_found" value="<? if($podestrian_id != null) { echo $podestrian->how_found;}?>"/>
                 </div>
 
 			</div>

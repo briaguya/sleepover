9872b70afbc0f81b190a5a594d0c41e07f1f1ec9
Brian Smith <briaguya@gmail.com>
2015-09-04 00:34:22 -0700

    autopopulate dates

--- application/views/booking/modify.php
+++ application/views/booking/modify.php
@@ -41,19 +41,15 @@
 
             <? if($booking_id == null) {?>
             <?="<div class=\"field\">" ?>
-                <?="<label for=\"checkin_datetime\">Checkin Date:</label>"?>
-                <?="<input type=\"date\" id=\"checkin_datetime\" name=\"checkin_datetime\" value=\""?><?=unix_to_human(now())?><?="\"/>"?>
-            <?="</div>"?>
-            <?="<div class=\"field\">" ?>
                 <?="<label for=\"checkin_datetime\">Checkin Date/Time:</label>"?>
-                <?="<input type=\"date\" id=\"checkin_datetime\" name=\"checkin_datetime\" value=\""?><?=unix_to_human(now())?><?="\"/>"?>
+                <?="<input type=\"datetime\" id=\"checkin_datetime\" name=\"checkin_datetime\" value=\""?><?=unix_to_human(now())?><?="\"/>"?>
             <?="</div>"?>
 
             <? }?>
 
             <div class="field">
                 <label for="checkout_date">Checkout Date:</label>
-                <input type="date" id="checkout_date" name="checkout_date" value="<? if($booking_id != null) { echo $booking->checkout_date; } else {echo unix_to_human(now());} ?>"/>
+                <input type="date" id="checkout_date" name="checkout_date" value="<? if($booking_id != null) { echo $booking->checkout_date; } else {echo date("Y-m-d");;} ?>"/>
             </div>
 
             <div class="login-actions">

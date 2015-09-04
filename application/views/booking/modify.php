a11a2fc94e20a91410038a887f5931d746d96cb1
Brian Smith <briaguya@gmail.com>
2015-09-04 01:37:47 -0700

    maybe now

--- application/views/booking/modify.php
+++ application/views/booking/modify.php
@@ -23,7 +23,7 @@
                 <?= "<select id=\"podestrian_id\" name=\"podestrian_id\">" ?>
                 <? foreach ($podestrians as $podestrian) { ?>
                     <?= "<option value=" ?>
-                    <?=$podestrian->podestrian_id?><?="\">"?>
+                    <?=$podestrian->podestrian_id?><?=">"?>
                     <?=$podestrian->first_name?><?= " " ?><?=$podestrian->last_name?>
                     <?= "</option>" ;}?>
                 <?= "</select>" ?>

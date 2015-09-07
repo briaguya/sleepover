9d9b41cd5896680eeb9c16ca57b42c8c0c1ff407
Brian Smith <briaguya@gmail.com>
2015-09-06 17:07:44 -0700

    yuck

--- application/views/booking/confirm.php
+++ application/views/booking/confirm.php
@@ -6,7 +6,7 @@
 
             <h1>Confirm Booking</h1>
             <h3><?=$pod->comboname?></h3>
-            <h3><?=$booking->checkin_date?> - <?=$booking->checkout_date?></h3>
+            <h3><?=$booking['checkin_date']?> - <?=$booking['checkout_date']?></h3>
 
             <div class=\"field\">
                 <label for=\"podestrian\">Podestrian:</label>
